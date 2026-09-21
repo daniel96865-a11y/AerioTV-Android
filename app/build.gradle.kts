cad09ed776c368f1d980f3b1b752746aa3ee00ec
daniel96865-a11y <daniel96865@googlemail.com>
2026-09-21 10:45:46 +0200

    Bump AerioTV Deutsch to 0.5.9-de2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "de.dgstudios.aeriotvde"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.5.9-de1"
+        versionCode = 2
+        versionName = "0.5.9-de2"
         vectorDrawables { useSupportLibrary = true }
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
