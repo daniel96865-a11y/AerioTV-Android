6b77cdd279b39c86d0062f9bc4d8292e3b60f78d
daniel96865-a11y <daniel96865@googlemail.com>
2026-09-21 16:49:56 +0200

    Bump Streamy to 3.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "de.dgstudios.aeriotvde"
         minSdk = 26
         targetSdk = 36
-        versionCode = 303
-        versionName = "3.0.3"
+        versionCode = 304
+        versionName = "3.0.4"
         vectorDrawables { useSupportLibrary = true }
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
