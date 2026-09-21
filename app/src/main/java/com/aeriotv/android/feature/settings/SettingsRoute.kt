12f6d51258b1d87a3daa73a470fd19be872ea10b
daniel96865-a11y <daniel96865@googlemail.com>
2026-09-21 18:34:20 +0200

    Replace escaped newlines in pairing routes

--- app/src/main/java/com/aeriotv/android/feature/settings/SettingsRoute.kt
+++ app/src/main/java/com/aeriotv/android/feature/settings/SettingsRoute.kt
@@ -120,7 +120,9 @@ fun visibleSettingsSections(
             add(SettingsSection.Appearance)
             add(SettingsSection.General)
             // Remote Control initiative: TV form factors only.
-            if (isTv) add(SettingsSection.RemoteControl)\n            add(SettingsSection.DevicePairing)\n            if (updaterEnabled) add(SettingsSection.AppUpdates)
+            if (isTv) add(SettingsSection.RemoteControl)
+            add(SettingsSection.DevicePairing)
+            if (updaterEnabled) add(SettingsSection.AppUpdates)
         },
     ),
     SettingsSectionGroupSpec(
@@ -152,7 +154,9 @@ fun settingsRouteForDeepLinkPage(
     "dvr" -> SettingsRoute.Section(SettingsSection.DvrSettings)
     "appearance" -> SettingsRoute.Section(SettingsSection.Appearance)
     "general" -> SettingsRoute.Section(SettingsSection.General)
-    "remote" -> SettingsRoute.Section(SettingsSection.RemoteControl)\n    "pairing", "sync" -> SettingsRoute.Section(SettingsSection.DevicePairing)\n    "updates" -> SettingsRoute.Section(SettingsSection.AppUpdates)
+    "remote" -> SettingsRoute.Section(SettingsSection.RemoteControl)
+    "pairing", "sync" -> SettingsRoute.Section(SettingsSection.DevicePairing)
+    "updates" -> SettingsRoute.Section(SettingsSection.AppUpdates)
     "developer" -> SettingsRoute.Section(SettingsSection.Developer)
     "about" -> SettingsRoute.About
     "playlist-detail" -> activePlaylistId?.let { SettingsRoute.PlaylistDetail(it) }
